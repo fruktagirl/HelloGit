0dde7385ec87fb54a63ab3d358a12d149e660a11
Emese Vincze <fruktagirl@yahoo.com>
2015-11-15 22:22:17 +0200

    Changes on github.

--- HelloGit/src/Main.java
+++ HelloGit/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		
 		System.out.println("Change and commit 2.");
 		
-		System.out.println("Change and commit 4. on github.");
+		System.out.println("Change and commit 4. on github..");
 
 	}
 
